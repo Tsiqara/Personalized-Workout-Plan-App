6c6a0530fe2f1a351b90dcf302da5fe8399db104
Tsiqara <mtsik21@freeuni.edu.ge>
2025-02-04 14:12:57 +0400

    Create custom errors.

---
+++ core/errors.py
@@ -0,0 +1,38 @@
+class UserDoesNotExistError(Exception):
+    pass
+
+
+class UserAlreadyExistsError(Exception):
+    pass
+
+
+class ExerciseDoesNotExistError(Exception):
+    pass
+
+
+class ExerciseAlreadyExistsError(Exception):
+    pass
+
+
+class WorkoutDoesNotExistError(Exception):
+    pass
+
+
+class WorkoutAlreadyExistsError(Exception):
+    pass
+
+
+class GoalDoesNotExistError(Exception):
+    pass
+
+
+class GoalAlreadyExistsError(Exception):
+    pass
+
+
+class WeightRecordDoesNotExistError(Exception):
+    pass
+
+
+class WeightRecordAlreadyExistsError(Exception):
+    pass
